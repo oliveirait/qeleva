864016753c9e5118bf88c3115310b5ef23f2b3e2
oliveirascof <oliveirascof@gmail.com>
2024-01-12 07:22:41 -0300

    refactor: rename repo

---
+++ src/components/header/index.tsx
@@ -0,0 +1,13 @@
+
+export const Header = () => {
+    return (
+        <nav className="fixed z-10 top-0 bg-slate-500 h-16 w-full items-center justify-center">
+            <div className="flex flex-row gap-4  h-14 items-center justify-center">
+                <h1>Inicio</h1>
+                <h1>Aulas</h1>
+                <h1>Contato</h1>
+                <h1>Sobre nos</h1>
+            </div>
+        </nav>
+    )
+}
